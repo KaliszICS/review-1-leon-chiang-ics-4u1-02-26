5c14af6271786468fa36be3f12d326b46649a79b
leon-chiang-ics-4u1-02-26 <Leon.Chiang@student.tdsb.on.ca>
2026-02-03 15:03:58 +0000

    solution

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,34 @@
 public class Review1 {
 
+
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		int num1;
+		int num2;
+		num1 = 5;
+		num2 = 9;
+		System.out.println(num2/num1);
+		System.out.println(num2%num1);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		System.out.println("true");
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		String x;
+		String y;
+		x="Hello";
+		y="World";
+		System.out.print(x + " ");
+		System.out.println(y);
 	}
 
 }
